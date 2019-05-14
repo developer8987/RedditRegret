b4c2153629e754bba6a7c793c09187cbd446779a
TheWomprat <therealwomprat@gmail.com>
2019-05-13 20:05:46 -0700

    Version 0.2

--- venv/RedditRegret.py
+++ venv/RedditRegret.py
@@ -7,36 +7,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
 
 #Constants
+website_url = "https://old.reddit.com"
 username_field_xpath = "//input[@placeholder='username']"
 password_field_xpath = "//input[@placeholder='password']"
 submit_button_xpath = "//button[text()='login']"
-username_button_xpath = "/html[1]/body[1]/div[2]/div[3]/span[1]/a[1]"
+username_button_xpath = "//span[@class='user']//a[contains(@href, 'https://old.reddit.com/user/')]"
 comments_link_xpath = "//a[contains(text(),'Comments')]"
+comments_error_xpath = "//div[@class='ProfileCommentsPage__error']"
 
-#WebDriver setup
+#WebDriver setup (you need to have Chromedriver installed, look it up)
 print "Setting up WebDriver..."
-try:
-    rp = 'firebug-2.0.19.xpi'
-    firefoxProfile = webdriver.FirefoxProfile()
-    firefoxProfile.add_extension(rp)
-except:
-    print "*** ERROR ***"
-    print "*** You need to have the 'firebug-2.0.19.xpi' file in the same directory as this script."
-    print "*** In order to build this file, simply go to https://github.com/firebug/firebug and"
-    print "*** navigate to the 'Build Firebug XPI' section, and follow the instructions to build the .xpi"
-    print "*** If the version is different, just change the 'rp = 'firebug-2.0.19.xpi'' line in this script"
-    print "*** to the version you have built. This program will now self-destruct. Have a blessed day."
-    print "*** ERROR ***"
-    exit()
-
-driver = webdriver.Firefox(firefox_profile=firefoxProfile)
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
 print "*** DONE ***"
 
 #Open Reddit
 print "Loading (old) Reddit site..."
-driver.get("https://old.reddit.com")
+driver.get(website_url)
 wait = WebDriverWait(driver,10)
 print "*** DONE ***"
 
@@ -46,10 +37,10 @@ try:
     username = sys.argv[1]
     password = sys.argv[2]
 except:
-    print "*** ERROR ***"
-    print "You must provide a username and a password as command line arguments. This program will now stop. "
-    print "Please rerun the script in the following manner: '~ python RedditRegret.py myusername mypassword' "
-    print "*** ERROR ***"
+    print """ *** ERROR ***
+    You must provide a username and a password as command line arguments. This program will now stop.
+    Please rerun the script in the following manner: '~ python RedditRegret.py myusername mypassword'
+    *** ERROR *** """
     exit()
 wait.until(EC.element_to_be_clickable((By.XPATH, username_field_xpath)))
 username_field = driver.find_element_by_xpath(username_field_xpath)
@@ -57,21 +48,9 @@ username_field.send_keys(username)
 wait.until(EC.element_to_be_clickable((By.XPATH, password_field_xpath)))
 password_field = driver.find_element_by_xpath(password_field_xpath)
 password_field.send_keys(password)
-
-
-
-
-time.sleep(5)
-
-
 wait.until(EC.element_to_be_clickable((By.XPATH, submit_button_xpath)))
 submit_button = driver.find_element_by_xpath(submit_button_xpath)
 submit_button.click()
-
-
-time.sleep(15)
-
-
 print "*** DONE ***"
 
 #Go to comment history
@@ -84,14 +63,20 @@ comments_link = driver.find_element_by_xpath(comments_link_xpath)
 comments_link.click()
 print "*** DONE ***"
 
-#Confirm to proceed
-print ""
-print "*** WARNING ***"
-print "You are about to erase your entire comment history! This cannot be undone!"
-print "*** WARNING ***"
-confirm_delete_all_comments = raw_input("Please type 'DELETE' in ALL CAPS to ERASE ALL COMMENTS: ")
-if confirm_delete_all_comments != "DELETE":
-	sys.exit()
+#Check if there are comments, and confirm to proceed
+try:
+    nocomments = driver.find_element_by_xpath(comments_error_xpath)
+    print "\n*** LOOKS LIKE YOU HAVE NO COMMENTS! EXITING! *** \n\nThank you for using RedditRegret."
+    sys.exit()
+except Exception:
+    print """ 
+    *** WARNING ***
+    You are about to erase your entire comment history! This cannot be undone!
+    *** WARNING *** """
+    confirm_delete_all_comments = raw_input("Please type 'DELETE' in ALL CAPS to ERASE ALL COMMENTS: ")
+    if confirm_delete_all_comments != "DELETE":
+	    print "GOOD CHOICE"
+	    sys.exit()
 
 #DELETE COMMENT LOOP
 print "Deleting comments..."
@@ -112,7 +97,11 @@ try:
         # time.sleep(0.2)
         counter = 1
 except NoSuchElementException:
-    print "Looks like we couldn't find a delete button."
-    print "Either you're all out of comments,"
-    print "...or we're all out of options. Thanks for using RedditRegret."
+    print """ 
+
+    *** DONE! ***
+
+    Looks like we couldn't find a delete button.
+    Either you're all out of comments,
+    ...or we're all out of options. Thanks for using RedditRegret. """
     exit()
